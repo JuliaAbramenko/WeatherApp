d18089d46ef2b3f406ce252ab95150b5fba6cdfa
Julia Abramenko <julia.abramenko@protonmail.com>
2023-06-06 09:07:46 +0200

    Defined OpenMeteoService interface

---
+++ app/src/main/java/com/example/weatherapp/api/OpenMeteoService.kt
@@ -0,0 +1,25 @@
+package com.example.weatherapp.api
+
+import com.example.weatherapp.data.model.Hourly
+import com.example.weatherapp.data.model.WeatherModel
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface OpenMeteoService {
+    @GET("v1/forecast")
+    suspend fun getWeather(
+        @Query("latitude")
+        latitude: Double,
+        @Query("longitude")
+        longitude: Double,
+        @Query("timezone")
+        timezone: String,
+        @Query("forecast_days")
+        forecastDays: Int,
+        @Query("windspeed_unit")
+        windspeedUnit: String,
+        @Query("current_weather")
+        currentWeatherModel: Boolean
+    ) : Response<WeatherModel>
+}
